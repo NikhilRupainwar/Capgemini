59b875b08fad762c7e3d220e55e175fd853e311f
NikhilRupainwar <43746612+NikhilRupainwar@users.noreply.github.com>
2018-10-05 10:33:43 +0530

    Add files via upload

---
+++ Lab11Question1/Lab11Question1/CreditCard.cs
@@ -0,0 +1,60 @@
+﻿//class 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11Question1
+{
+     public delegate void CreditCardDelegate(string message);
+
+    public class CreditCard
+    {
+        public event CreditCardDelegate CredEvent;
+
+        public int Balance { get; set; }
+
+        // parameterised constructor
+        public CreditCard(int balance)
+        {
+            Balance = balance;
+        }
+
+        public int GetBalance(int balance)
+        {
+            Balance = balance;
+            return balance;
+        }
+
+        public int CreditLimit { get; set; }
+
+        public int GetCreditLimit(int creditlimit)
+        {
+            CreditLimit = creditlimit;
+            return creditlimit;
+        }
+
+        public void MakePayment(int amount)
+        {
+            string message = "";
+           
+                if (Balance > amount)
+                {
+                    Balance = Balance - amount;
+                    message = "After deducting payment " + amount + "remaining balance is " + Balance;
+                }
+                else
+                {
+                    message = "You are trying to pay greater amount than your balance";
+                }
+            
+        }
+
+        public void Notify(string message)
+        {
+            if (CredEvent != null)
+                CredEvent(message);
+        }
+    }
+}
---
+++ Lab11Question1/Lab11Question1/Program.cs
@@ -0,0 +1,40 @@
+﻿// console application
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text; 
+using System.Threading.Tasks;
+
+namespace Lab11Question1
+{
+   
+class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditCard c = new CreditCard(10000);
+          
+            Console.WriteLine("Please enter the amount for making payment :");
+            int amount = Convert.ToInt32(Console.ReadLine());
+
+            string message = "";
+           
+                if (c.Balance > amount)
+                {
+                    c.MakePayment(amount);
+                    message = "After deducting : " + amount + ", remaining balance is : " + c.Balance;
+                }
+                else
+                {
+                    message = "You are trying to withdraw greater amount than your balance";
+                }
+            
+
+            c.MakePayment(amount);
+            c.Notify(message);
+            Console.WriteLine(message);
+
+            Console.ReadKey();
+        }
+    }
+}
